47a185be17b0bc2cd849120fbd0b2c7ac5211cc2
Yessenia <ychaiu@gmail.com>
2019-03-24 21:52:15 -0700

    add links to sidebar

--- client/src/containers/Sidebar.js
+++ client/src/containers/Sidebar.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Image from 'react-bootstrap/Image';
 
 class Sidebar extends Component {
     render() {
         return (
-            <div>hello!</div>
+            <div>
+                <Nav className="flex-column">
+                    <Image src="holder.js/171x180" roundedCircle />
+                    <Nav.Link href ="/About">About Me</Nav.Link>
+                    <Nav.Link href ="/Portfolio">Portfolio</Nav.Link>
+                    <Nav.Link href ="/Resume">Resume</Nav.Link>
+                    <Nav.Link href ="/Blog">Blog</Nav.Link>
+                </Nav>
+            </div>
         )
     }
 }
